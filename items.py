b3b0dd421b6cd4b9779bf8278790a79c0eff5ca4
m0ester <106405841+m0ester@users.noreply.github.com>
2026-01-20 18:53:09 +0000

    Add files via upload

---
+++ items.py
@@ -0,0 +1,20 @@
+from typing import Any, NamedTuple, TYPE_CHECKING
+
+
+class WwItem(Item):
+    game: = str"Warioworld"
+	
+class ItemType(Enum):
+	SPRITELING = 0
+	TREASURE = 1
+	RED_DIAMOND = 2
+	BOSS_MEDAL = 3
+	ENTRANCE = 4
+	STATUE_PIECE = 5
+
+class WwItemData(NamedTuple):
+    name: str
+    classification: int
+    address: int
+    quantity: int
+    item_id: Optional[int]
