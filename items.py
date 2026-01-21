0b0b545ef5aa24fd3247665eb0f5ffd6f5c2296a
m0ester <mohamed.saidoune@hotmail.com>
2026-01-21 16:35:57 +0000

    finishing up required items file stuff

--- items.py
+++ items.py
@@ -1,7 +1,7 @@
 from typing import Any, NamedTuple, TYPE_CHECKING
 from enum import Enum
-from BaseClassees import ItemClassification as IC
-from gamedata import Treasure, Spriteling, BossMedal, StageDoor
+from BaseClasses import ItemClassification as IC
+import gamedata
 	
 class ItemType(Enum):
 	SPRITELING = 0
@@ -10,8 +10,18 @@ class ItemType(Enum):
 	BOSS_MEDAL = 3
 	DOOR = 4
 	STATUE_PIECE = 5
+    OTHER = 6
 
 class WwItemData(NamedTuple):
     type: str
     classification: IC
     quantity: int
+    address: int = 0
+    item_type: ItemType = ItemType.OTHER
+
+def get_spriteling(spriteling: str, stage: str) -> str:
+      return f"(spriteling) in (stage)"
+
+data_table: list[WwItemData] = [
+      
+]
