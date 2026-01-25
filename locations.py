bdf69d56ce20dcef5284d7c5b8ec2a0019ef54bb
m0ester <mohamed.saidoune@hotmail.com>
2026-01-25 19:59:25 +0000

    added locations basics

---
+++ locations.py
@@ -0,0 +1,21 @@
+from typing import NamedTuple, TYPE_CHECKING
+from enum import Enum, Flag
+from BaseClasses import Location
+from gamedata import CHECK_TABLE, Chest, Cage, BossBeat
+
+class WwLocation(Location):
+    game: str = "Warioworld"
+
+    def __init__(self, player: int, name: str, data: Chest | Cage | BossBeat):
+        address = None if data.CheckID is None else address = data.loc
+        name=data.name
+        super().__init__(player, name, address)
+
+        self.code = data.CheckID
+        if isinstance(data.name, BossBeat):
+            self.stage_id = data.name.split()[1:]
+        else:
+            self.stage_id = data.name.split()[:2]
+        #self.region = self.code
+        self.bit = data.value
+        self.address = self.address
