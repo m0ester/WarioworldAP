a4f0960e0042030a7116fc8c9443336f4b0096b3
m0ester <mohamed.saidoune@hotmail.com>
2026-02-01 22:00:31 +0000

    removed unnecessary definitions

--- locations.py
+++ locations.py
@@ -1,35 +1,19 @@
-from Cython.Shadow import address
 from BaseClasses import Location
-from .gamedata import Chest, Cage, BossBeat
-import inspect
+from .gamedata import CHECK
+from .regions import *
+
 class WwLocation(Location):
     game: str = "Warioworld"
 
-    def __init__(self, player: int, name: str, data: Chest | Cage | BossBeat):
-        address = None if data.CheckID is None else WwLocation.get_apid(data.CheckID)
-        super().__init__(player, name, address=address)
-        #PUT REGIONS IN THE GAMEDATA CHECKS
-        self.code = data.CheckID
-        self.region = data.region
-        self.bit = data.memvalue
-        self.address = self.address
-    @staticmethod
-    def get_apid(code: int) -> int:
+ #   def __init__(self, player: int, name: str, parent: Region, data: CHECK):
+  #      address = None if data.CheckID is None else WwLocation.get_apid(data.CheckID)
+   #     super().__init__(player, name, address=address)
+    #    self.code = data.CheckID
+     #   self.region = data.region
+      #  self.bit = data.memvalue
+       # self.address = data.memloc
 
-        base_id=2326528
-        return base_id + code
+    #@staticmethod
+    #def get_apid(code: int) -> int:
+     #   return code
 
-boss_to_stage: dict [str, str] ={
-        "Defeated Greenfist" : "Greenhorn Forest",
-        "Defeated Sandworm" : "Greenhorn Ruins",
-        "Defeated Dinomighty" : "Dinomighty's Showdown",
-        "Defeated Brawl Doll":  "Horror Manor",
-        "Defeated Clown-a-Round":  "Wonky Circus",
-        "Defeated Dual Dragon":     "Dual Dragon's Showdown",
-        "Defeated Winter Windster": "Shivering Mountains",
-        "Defeated Spideraticus":    "Beanstalk Way",
-        "Defeated Red-Brief J":     "Red-Brief J's Showdown",
-        "Defeated The Mean Emcee":  "Mirror Mansion",
-        "Defeated Ironsider":       "Pecan Sands",
-        "Defeated Captain Skull":   "Captain Skull's Showdown",
-}
