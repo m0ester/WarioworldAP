77e3172726efbf41f787a2d8305bcf200daffd1a
m0ester <mohamed.saidoune@hotmail.com>
2026-02-15 21:05:49 +0000

    fixed completion condition

--- Rules.py
+++ Rules.py
@@ -145,5 +145,5 @@ def set_rules(world):
             pass
 
     #world.get_location("Victory").access_rule = lambda state: False
-    world.multiworld.completion_condition[world.player] = lambda state: state.has("Victory", world.player)
+    world.multiworld.completion_condition[world.player] = lambda state: state.can_reach_location("VictoryLoc", world.player)
     #and WwOptions.ending == SpritelingsCollected)
